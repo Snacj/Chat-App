f5becb760d00a3a87aa97cc9b00b2f603182e45c
snacj <0xsnacj@proton.me>
2025-06-19 18:32:36 +0200

    changed filestructure

--- src/index.js
+++ src/index.js
@@ -48,11 +48,11 @@ if (cluster.isPrimary) {
     const __dirname = dirname(fileURLToPath(import.meta.url));
 
     app.get('/', (req, res) => {
-        res.sendFile(join(__dirname, 'index.html'));
+        res.sendFile(join(__dirname, '../public/index.html'));
     });
 
     app.get('/chat', (req, res) => {
-        res.sendFile(join(__dirname, 'chat.html'));
+        res.sendFile(join(__dirname, '../public/chat.html'));
     });
 
     io.on('connection', async (socket) => {
